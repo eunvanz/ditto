b4ffc5946b5a53a06371b469040d26a6ddfe8c37
Benjamin <eunvanz@naver.com>
2020-09-22 04:14:48 +0900

    feat: firebase 초기화

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiyEpGtkhrtsRWLYdjA5A8ECyeOf1MlJY",
+  authDomain: "ditto-e21bc.firebaseapp.com",
+  databaseURL: "https://ditto-e21bc.firebaseio.com",
+  projectId: "ditto-e21bc",
+  storageBucket: "ditto-e21bc.appspot.com",
+  messagingSenderId: "738188473066",
+  appId: "1:738188473066:web:655ac9c484277d3f695d93",
+  measurementId: "G-H2YRKPH7XS",
+};
+
+firebase.initializeApp(firebaseConfig);
