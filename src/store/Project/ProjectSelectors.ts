06a2dbd945b85af9fe99e7eff9c68658ba3b4496
Benjamin <eunvanz@naver.com>
2020-10-20 02:28:50 +0900

    fix: ProjectSelectors 타입추론 가능하게 변경

--- src/store/Project/ProjectSelectors.ts
+++ src/store/Project/ProjectSelectors.ts
@@ -2,69 +2,49 @@ import { RootState } from "..";
 import { ProjectActions } from "./ProjectSlice";
 import { createSelector } from "@reduxjs/toolkit";
 import { DATA_KEY } from "../Data/DataSlice";
-import DataSelectors from "../Data/DataSelectors";
-import { ProjectUrlDoc, ProjectDoc, ModelDoc } from "../../types";
 import { convertRecordToArray } from "../../helpers/commonHelpers";
 
 const selectIsProjectFormSubmitting = (state: RootState) =>
   state.progress.includes(ProjectActions.submitProjectForm.type);
 
 const selectProjectUrls = createSelector(
-  DataSelectors.createDataKeySelector(DATA_KEY.PROJECT),
-  DataSelectors.createDataKeySelector(DATA_KEY.PROJECT_URLS),
+  (state: RootState) => state.data[DATA_KEY.PROJECT],
+  (state: RootState) => state.data[DATA_KEY.PROJECT_URLS],
   (project, projectUrls) => {
-    return projectUrls
-      ? (projectUrls as Record<string, ProjectUrlDoc[]>)[
-          (project as ProjectDoc).id
-        ]
-      : undefined;
+    return projectUrls && project ? projectUrls[project.id] : undefined;
   }
 );
 
 const selectProjectModels = createSelector(
-  DataSelectors.createDataKeySelector(DATA_KEY.PROJECT),
-  DataSelectors.createDataKeySelector(DATA_KEY.MODELS),
+  (state: RootState) => state.data[DATA_KEY.PROJECT],
+  (state: RootState) => state.data[DATA_KEY.MODELS],
   (project, models) => {
-    return models
-      ? convertRecordToArray(
-          (models as Record<string, Record<string, ModelDoc>>)[
-            (project as ProjectDoc).id
-          ]
-        )
+    return models && project
+      ? convertRecordToArray(models[project.id])
       : undefined;
   }
 );
 
 const selectForProjectBasicForm = createSelector(
-  DataSelectors.createDataKeySelector(DATA_KEY.PROJECT),
+  (state: RootState) => state.data[DATA_KEY.PROJECT],
   selectIsProjectFormSubmitting,
   (project, isSubmitting) => ({
-    project: (project as ProjectDoc) || undefined,
+    project,
     isSubmitting,
   })
 );
 
 const createModelFormSelector = (modelFormId?: string) =>
   createSelector(
-    DataSelectors.createDataKeySelector(DATA_KEY.MODELS),
-    DataSelectors.createDataKeySelector(DATA_KEY.PROJECT),
-    DataSelectors.createRecordDataKeySelector({
-      dataKey: DATA_KEY.MODEL_FORMS,
-      recordKey: modelFormId || "",
-    }),
+    (state: RootState) => state.data[DATA_KEY.MODELS],
+    (state: RootState) => state.data[DATA_KEY.PROJECT],
+    (state: RootState) => state.data[DATA_KEY.MODEL_FORMS]?.[modelFormId || ""],
     (models, project, modelId) => {
       const model =
-        models && project && modelId
-          ? (models as Record<string, any>)[
-              (project as ProjectDoc).id as string
-            ][modelId as string]
-          : undefined;
+        models && project && modelId ? models[project.id][modelId] : undefined;
       const existingModelNames: string[] = [];
-      if (models) {
-        const projectModels = (models as Record<
-          string,
-          Record<string, ModelDoc>
-        >)[(project as ProjectDoc).id];
+      if (models && project) {
+        const projectModels = models[project.id];
         convertRecordToArray(projectModels).forEach((item) => {
           if (!modelId || item.id !== modelId) {
             existingModelNames.push(item.name);
