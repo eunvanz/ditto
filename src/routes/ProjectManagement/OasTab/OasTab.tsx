bb03f778c9365a1cdb040c449356e2c22db872bf
Benjamin <eunvanz@naver.com>
2021-03-24 21:11:45 +0900

    fix: ace editor 테마 변경

--- src/routes/ProjectManagement/OasTab/OasTab.tsx
+++ src/routes/ProjectManagement/OasTab/OasTab.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, CardHeader, Divider } from "@material-ui/core";
-import "ace-builds/src-noconflict/mode-yaml";
-import "ace-builds/src-noconflict/theme-github";
-import "ace-builds/src-noconflict/theme-nord_dark";
 import Editor from "react-ace";
 import YAML from "yaml";
 import { Oas, THEMES } from "../../../types";
+import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/theme-chrome";
+import "ace-builds/src-noconflict/theme-nord_dark";
 
 export interface OasTabProps {
   data?: Oas;
@@ -19,7 +19,7 @@ export const OasTab: React.FC<OasTabProps> = ({ data, theme }) => {
       <Divider />
       <Editor
         mode="yaml"
-        theme={theme === THEMES.LIGHT ? "github" : "nord_dark"}
+        theme={theme === THEMES.LIGHT ? "chrome" : "nord_dark"}
         value={YAML.stringify(JSON.parse(JSON.stringify(data)))}
         width="100%"
         height={`calc(100vh - 404px)`}
