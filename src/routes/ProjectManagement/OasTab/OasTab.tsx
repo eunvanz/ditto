24258674e8980945d1fae17232c1dfa343f8f8c4
Benjamin <eunvanz@naver.com>
2021-03-24 20:56:26 +0900

    fix: Editor height 조정

--- src/routes/ProjectManagement/OasTab/OasTab.tsx
+++ src/routes/ProjectManagement/OasTab/OasTab.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card, CardHeader, Divider } from "@material-ui/core";
-import { Oas, THEMES } from "../../../types";
-import YAML from "yaml";
-import Editor from "react-ace";
-import "ace-builds/src-noconflict/theme-nord_dark";
-import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/theme-nord_dark";
+import Editor from "react-ace";
+import YAML from "yaml";
+import { Oas, THEMES } from "../../../types";
 
 export interface OasTabProps {
   data?: Oas;
@@ -22,7 +22,7 @@ export const OasTab: React.FC<OasTabProps> = ({ data, theme }) => {
         theme={theme === THEMES.LIGHT ? "github" : "nord_dark"}
         value={YAML.stringify(JSON.parse(JSON.stringify(data)))}
         width="100%"
-        height={`calc(100vh - 400px)`}
+        height={`calc(100vh - 404px)`}
         setOptions={{ fixedWidthGutter: true }}
       />
     </Card>
