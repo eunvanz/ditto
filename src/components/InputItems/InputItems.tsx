7f649b5955cf5357b5a41c7661a1d49679aecb63
Benjamin <eunvanz@naver.com>
2021-04-15 01:01:59 +0900

    feat: InputItems에 add 버튼 추가

--- src/components/InputItems/InputItems.tsx
+++ src/components/InputItems/InputItems.tsx
@@ -1,10 +1,36 @@
-import React, { KeyboardEvent, useCallback } from "react";
-import { Box, Chip, makeStyles, TextField, TextFieldProps } from "@material-ui/core";
+import React, { KeyboardEvent, useCallback, useState } from "react";
+import {
+  Box,
+  Button,
+  Chip,
+  makeStyles,
+  Paper,
+  TextField,
+  TextFieldProps,
+  Typography,
+} from "@material-ui/core";
+import clsx from "clsx";
 import { Theme } from "../../theme";
 
 const useStyles = makeStyles((theme: Theme) => ({
   itemChip: {
     marginRight: theme.spacing(1),
+  },
+  textWrapper: {
+    display: "flex",
+  },
+  addButton: {
+    marginLeft: theme.spacing(1),
+    width: 100,
+    height: 56,
+    "&.flat": {
+      width: 80,
+      height: 28,
+    },
+  },
+  itemsPaper: {
+    padding: theme.spacing(1),
+    background: theme.palette.background.dark,
   },
 }));
 
@@ -20,6 +46,7 @@ export const InputItems: React.FC<InputItemsProps> = ({
   items,
   onAddItem,
   onDeleteItem,
+  variant,
   ...restProps
 }) => {
   const classes = useStyles();
@@ -34,9 +61,11 @@ export const InputItems: React.FC<InputItemsProps> = ({
     [onAddItem],
   );
 
+  const [buttonColor, setButtonColor] = useState<"secondary" | undefined>(undefined);
+
   return (
     <>
-      <Box>
+      <Box className={classes.textWrapper}>
         <TextField
           onKeyDown={(e) => {
             // 엔터키로 form submit 하지 않음
@@ -46,19 +75,38 @@ export const InputItems: React.FC<InputItemsProps> = ({
           }}
           onKeyUp={handleOnKeyUp}
           autoComplete="off"
+          variant={variant}
+          onFocus={() => setButtonColor("secondary")}
+          onBlur={() => setButtonColor(undefined)}
           {...restProps}
         />
+        <Button
+          onClick={onAddItem}
+          className={clsx(classes.addButton, { flat: !variant })}
+          variant="contained"
+          color={buttonColor}
+        >
+          Add
+        </Button>
       </Box>
       <Box mt={1}>
-        {items?.map((item) => (
-          <Chip
-            key={item}
-            className={classes.itemChip}
-            color="default"
-            label={item}
-            onDelete={() => onDeleteItem(item)}
-          />
-        ))}
+        <Paper className={classes.itemsPaper} elevation={0}>
+          {items?.length ? (
+            items.map((item) => (
+              <Chip
+                key={item}
+                className={classes.itemChip}
+                color="default"
+                label={item}
+                onDelete={() => onDeleteItem(item)}
+              />
+            ))
+          ) : (
+            <Typography component="i" color="textSecondary">
+              No added items
+            </Typography>
+          )}
+        </Paper>
       </Box>
     </>
   );
